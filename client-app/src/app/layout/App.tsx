7f72d3bb6595d76c1e3f2ba29d8b8812d73fc018
Teuta Krasniqi <tk48167@ubt-uni.net>
2022-06-15 19:10:14 +0200

    Adding functionalities on Details and style changes

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,51 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+   
+function App() {
+  const [activities, setActivitites] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+  //it takes no parameters
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      
+      setActivitites(response.data);
+      
+    })
+  }, []);
+
+  function handleSelectActivity(id: string){
+    //x is a variable which is gonna hold the activity 
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string){
+    
+  }
+
+  return (
+    <>
+     <NavBar />
+      <Container style={{marginTop:'7em'}}>
+        {/* is passing activities down as properties */}
+        <ActivityDashboard 
+        activities={activities}
+        selectedActivity={selectedActivity}
+        selectActivity = {handleSelectActivity}
+        cancelSelectActivity={handleCancelSelectActivity}
+        />
+      </Container>
+    </>
+  );
+}
+
+export default App;
