9e04a12f490b1246f36b2ea1ba17cd5abc43b8a6
Teuta Krasniqi <tk48167@ubt-uni.net>
2022-06-16 20:02:16 +0200

    Adding delete functionality to delete button

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
    
 function App() {
-  const [activities, setActivitites] = useState<Activity[]>([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   const [editMode, setEditMode] = useState(false);
@@ -15,7 +16,7 @@ function App() {
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
       
-      setActivitites(response.data);
+      setActivities(response.data);
       
     })
   }, []);
@@ -37,8 +38,18 @@ function App() {
   function handleFormClose(){
     setEditMode(false);
   }
+  //we are gonna pass an activity as parameter of type of Activity then we check the id
+  function handleCreateOrEditActivity(activity: Activity){
+    activity.id 
+     ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+     : setActivities([...activities, {...activity, id: uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+    }
 
-
+    function handleDeleteActivity(id: string){
+      setActivities([...activities.filter(x => x.id !== id )])
+    }
 
   return (
     <>
@@ -53,6 +64,8 @@ function App() {
         editMode={editMode}
         openForm={handleFormOpen}
         closeForm={handleFormClose}
+        createOrEdit={handleCreateOrEditActivity}
+        deleteActivity={handleDeleteActivity}
         />
       </Container>
     </>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
@@ -13,6 +13,8 @@ interface Props{
     editMode:boolean;
     openForm:(id : string) => void;
     closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 export default function ActivityDashboard({
      activities,
@@ -21,11 +23,15 @@ export default function ActivityDashboard({
      cancelSelectActivity,
     editMode,
     openForm,
-    closeForm}:Props){
+    closeForm,
+    createOrEdit,
+    deleteActivity}:Props){
     return(
         <Grid>
             <Grid.Column width='10'>
-               <ActivityList activities={activities} selectActivity={selectActivity}/>
+               <ActivityList activities={activities} 
+               selectActivity={selectActivity}
+               deleteActivity={deleteActivity}/>
             </Grid.Column>
             <Grid.Column width="6">
                 {selectedActivity && !editMode &&
@@ -33,7 +39,7 @@ export default function ActivityDashboard({
                                  cancelSelectActivity={cancelSelectActivity}
                                  openForm={openForm} />}
                                  {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity}/>}
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
             </Grid.Column>
         </Grid>
     )
