1e06aabe9ea8917c27c7cc71062ee2526d430088
Teuta Krasniqi <tk48167@ubt-uni.net>
2022-06-19 10:14:01 +0200

    CRUD operations on server side

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props{
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 export default function ActivityDashboard({
      activities,
@@ -25,21 +26,33 @@ export default function ActivityDashboard({
     openForm,
     closeForm,
     createOrEdit,
-    deleteActivity}:Props){
+    deleteActivity,
+    submitting}:Props){
     return(
         <Grid>
             <Grid.Column width='10'>
                <ActivityList activities={activities} 
                selectActivity={selectActivity}
-               deleteActivity={deleteActivity}/>
+               deleteActivity={deleteActivity}
+               submitting={submitting}
+               />
             </Grid.Column>
             <Grid.Column width="6">
                 {selectedActivity && !editMode &&
-                <ActivityDetails activity={selectedActivity} 
-                                 cancelSelectActivity={cancelSelectActivity}
-                                 openForm={openForm} />}
-                                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+
+                <ActivityDetails 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm={openForm} 
+                />}
+                {editMode &&
+
+                <ActivityForm 
+                    closeForm={closeForm}
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}
+                />}
             </Grid.Column>
         </Grid>
     )
